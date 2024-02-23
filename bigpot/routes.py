f14add5ef782265361bf11ada42b541d536bccc0
Husnain Raza <hr_11@hotmail.co.uk>
2024-02-23 22:43:27 +0000

    added new recipe index.html

--- bigpot/routes.py
+++ bigpot/routes.py
@@ -1,10 +1,15 @@
+import os
+import json
 from flask import render_template
 from bigpot import app, db
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data = []
+    with open("data/recipies.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("index.html", recipies=data)
 
 @app.route("/about")
 def about():
@@ -12,8 +17,8 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title='Contact')
 
 @app.route("/community")
 def community():
-    return render_template("community.html")
+    return render_template("community.html", page_title='Community')
