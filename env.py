83d70df022092ceae6fd834b56aa493477fb3c47
Husnain Raza <hr_11@hotmail.co.uk>
2024-04-23 13:25:39 +0000

    updated repository

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "cook_it")
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("DEVELOPMENT", "True")
-os.environ.setdefault("DEBUG", "FALSE")
-os.environ.setdefault("DB_URL", "postgresql:///community")
